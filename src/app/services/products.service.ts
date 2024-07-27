902b8d12be9396617cb114114095d68f9b9c9e4a
Justin <justinvargas4299@gmail.com>
2024-07-26 20:43:13 -0600

    Avance fecth y crud

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+
+  getProducts(){
+
+  }
+  getOne(){
+    
+  }
+  save(){
+
+  }
+  update(){
+
+  }
+  delete(){
+
+  }
+  constructor() { }
+}
